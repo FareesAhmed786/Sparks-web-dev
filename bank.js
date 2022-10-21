490ea42465c43f914a25f90f8132455d3ef2b438
Farees Ahmed Shaikh <75060030+FareesAhmed786@users.noreply.github.com>
2022-10-21 22:17:57 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,21 @@
+document.getElementById("login-submit").addEventListener('click', function () {
+    const emailField = document.getElementById("user-email");
+    const userEmail = emailField.value;
+
+    const passwordField = document.getElementById("user-password");
+    const userPassword = passwordField.value;
+
+    if (userEmail == 'Farees@gmail.com' && userPassword == 'bank') {
+        const para = document.getElementById("pass-check")
+        para.innerText = '✅ Access Given';
+        window.location.href = 'banking.html';
+    }
+    else {
+        const para = document.getElementById("pass-check")
+        para.innerText = '❌ Access Denied, Try Again';
+
+    }
+
+    emailField.value = "";
+    passwordField.value = "";
+})
